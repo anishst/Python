8eb29040567b88e13d5ebae517ef7848aa2cfc56
sebastian-anish <sebastian_anish@bah.com>
2019-05-23 13:26:04 -0400

    updates

--- delete_files_older_than_certain_days.py
+++ delete_files_older_than_certain_days.py
@@ -1,20 +1,27 @@
-"""Deletes the log files under a directory that are older than
+"""Deletes the files with provided extensions under a directory that are older than
 provided number of days"""
 import os, time
 
-top = r'C:\Temp\SeleniumPythonFramework'
+root = r'C:\Temp\SeleniumPythonFramework'
+file_extensions_to_remove = ('.log', '.png') # tuple
+how_many_days_old_logs_to_remove = 1
+removed_files = []
 
-for dirpath, dirnames, filenames in os.walk(top):
+
+for dirpath, dirnames, filenames in os.walk(root):
 	print(f"Checking Folder: {dirpath}")
 	folder_path = dirpath
-	file_ends_with = ".log"
-	how_many_days_old_logs_to_remove = 1
+
 	now = time.time()
 
 	for file in os.listdir(folder_path):
 	    file_full_path = os.path.join(folder_path,file)
-	    if os.path.isfile(file_full_path) and file.endswith(file_ends_with):
+	    if os.path.isfile(file_full_path) and file.endswith(file_extensions_to_remove):
 	        #Delete files older than x days
 	        if os.stat(file_full_path).st_mtime < now - how_many_days_old_logs_to_remove * 86400: 
 	             os.remove(file_full_path)
 	             print ("File Removed : " , file_full_path)
+	             removed_files.append(file_full_path)
+
+print(f"{len(removed_files)} files were removed from {root}")
+print(removed_files)
