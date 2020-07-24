8feb7d37e01141969e919dc4281aee749ac7c0bc
Anish Sebastian <anishst@hotmail.com>
2020-07-24 09:22:57 -0400

    added mortgage_rates_alert.py

---
+++ mortgage_rates_alert.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from selenium import webdriver
+from ATSFramework import EmailFL
+
+driver = webdriver.Chrome()
+#  list of sites and xpath to latest article links
+url = "https://www.bankrate.com/rates/interest-rates/prime-rate.aspx"
+xpath = """//*[@id="csstyle"]/div[6]/main/div[1]/table/tbody/tr[1]/td[2]"""
+current_rate = ""
+
+try:
+    driver.get(url)
+    print(url, xpath)
+    time.sleep(2)
+    table = driver.find_element_by_xpath(xpath)
+    current_rate = float(table.text)
+    print(f"Current WSJ Prime Rate: {table.text}")
+except Exception as e:
+    print(f"Something went wrong while scraping {url} {e} ")
+
+if current_rate <= 3.00:
+    message = f"""
+    <a href='https://www.bankrate.com/rates/interest-rates/prime-rate.aspx'>WSJ Prime Rate seems </a> to have gone down.
+    <p>Current WSJ Prime Rate: {table.text}</p>"""
+    #  send email
+    EmailFL.send_email_gmail([os.getenv('GMAIL_ID')], "WSJ Mortgage Rate is at or below 3%", message)
+
+driver.quit()
